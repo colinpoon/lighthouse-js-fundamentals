e477d15621b435ebd6ff2c27b29fc3b76231cd9f
Colin Poon <me@colinpoon.com>
2021-07-28 10:44:48 +0000

    reup on ingredients commit

--- ingredients.js
+++ ingredients.js
@@ -1,7 +1,21 @@
 const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
 
 // Write a while loop that prints out the contents of ingredients:
-
+let bananaBread = 1;
+while (bananaBread.length < 9) {
+    console.log(bananaBread);
+    bananaBread++;
+}
 // Write a for loop that prints out the contents of ingredients:
-
+for (let i = 0; i < ingredients.length; i++) {
+    console.log(ingredients[i]);
+}
 // Write any loop (while or for) that prints out the contents of ingredients backwards:
+for (let r = 9; r >= ingredients.length; r--) {
+    console.log(ingredients[r])
+}
+let bananaBread = 1;
+while (bananaBread.length > 9) {
+    console.log(bananaBread);
+    bananaBread--;
+}
