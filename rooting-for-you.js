ca137084b04f8bc7c3c203080ca4ac3c946fadb5
Colin Poon <me@colinpoon.com>
2021-08-09 04:45:54 +0000

    adding rooting for you code evaluation

--- rooting-for-you.js
+++ rooting-for-you.js
@@ -1,3 +1,36 @@
-const judgeVegetable = function(vegetables, metric) {
+const vegetables = [{
+        submitter: 'Old Man Franklin',
+        redness: 10,
+        plumpness: 5
+    },
+    {
+        submitter: 'Sally Tomato-Grower',
+        redness: 2,
+        plumpness: 8
+    },
+    {
+        submitter: 'Hamid Hamidson',
+        redness: 4,
+        plumpness: 3
+    }
+]
+const metric = 'redness'
 
-}
+// We want to find loop through the object array and determine: //
+// The name of the submitter of the BEST vegetables //
+// Best in redness OR plumpness //
+
+const judgeVegetable = function(vegetables, metric) {
+    var best = 0;
+    var bestInShow;
+
+    for (var i = 0; i < vegetables.length; i++) {
+
+        if (vegetables[i][metric] > best) {
+            best = vegetables[i][metric];
+            bestInShow = vegetables[i].submitter;
+        }
+    }
+    return bestInShow;
+};
+console.log(judgeVegetable);
